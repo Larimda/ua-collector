4e62275db195c9b26497de6e9dc1192b2d54f9d0
陈锋 <chenf@shinemo.com>
2015-11-05 11:53:24 +0800

    1. 连接数据库 2. 添加记录页面 3. 记录结果列表页面

---
+++ db.js
@@ -0,0 +1,11 @@
+var mysql = require('mysql');
+
+var dbPool = mysql.createPool({
+	connectionLimit : 10,
+	host: '127.0.0.1',
+	user: 'root',
+	password: 'test',
+	database: 'ua-collector'
+});
+
+module.exports = dbPool;
--- routes/ua-collector.js
+++ routes/ua-collector.js
@@ -1,24 +1,56 @@
 var express = require('express');
 var router = express.Router();
+var dbPool = require('../db.js');
 
-router.get('/', function(req, res, next) {
-  res.render('ua-collector', {
-  	title: 'useragent收集器'
+router.all('/add', function(req, res, next) { // 首页，支持post
+  res.render('ua-collector/add', {
+    title: 'useragent收集器'
   });
 });
 
-router.post('/', function(req, res, next) {
-  res.render('ua-collector', {
-  	title: 'useragent收集器'
+router.all('/list', function(req, res, next) { // 结果页，支持post
+  var sql = 'select * from `records`';
+  dbPool.getConnection(function(err, connection){
+    var query = connection.query(sql, function(err, rows, fields){
+      if(!err){
+        res.render('ua-collector/list', {
+          rows: rows
+        });
+      }else{
+        res.render('error', {
+          message: '页面错误',
+          error: err
+        });
+      }
+
+      connection.release();
+    });
   });
 });
 
-router.post('/records', function(req, res, next){
-	res.json({
-		success: true,
-		code: 200,
-		msg: '操作成功'
-	});
+router.post('/records', function(req, res, next){ // post --> /ua-collector/records 采集信息
+  var post = req.body;
+  var sql = 'INSERT INTO `records` set ?';
+
+  dbPool.getConnection(function(err, connection){
+    var query = connection.query(sql, post, function(err, rows, fields){
+      if(!err){
+        res.json({
+          success: true,
+          code: 200,
+          msg: '执行成功'
+        })
+      }else{
+        res.json({
+          success: false,
+          code: 500,
+          msg: '执行失败'
+        });
+      }
+
+      connection.release();
+    });
+  });
 });
 
 module.exports = router;
